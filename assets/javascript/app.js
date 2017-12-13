cf7bb248c87817fb0b8976e05f3e4eac2891a0d6
Mark Powalisz <markpowalisz@Marks-MacBook-Pro.local>
2017-12-13 12:53:43 -0500

    12/13 12:53

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,25 @@
+
+
 $(document).ready(function () {
 
+// Score Counter
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var answer = [
+    "banshee",
+    "llama",
+    "burrito"
+]
+
+// 
 var button1 = document.getElementById("button1");
 var button2 = "";
-
+// Button Function
     $(".btn").on("click", function () {
-                
-        console.log(button1);
+        $(".btn").text(answer[0]);
+        correctAnswers++;
+        console.log(correctAnswers);
     })
 
 })
+
