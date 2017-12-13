d1dbe6d2af6b211fc232f082c471206e58f54ad7
Mark Powalisz <markpowalisz@Marks-MacBook-Pro.local>
2017-12-12 22:44:57 -0500

    12/12 22:44

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+var button1 = document.getElementById("button1");
+var button2 = "";
+
+    $(".btn").on("click", function () {
+                
+        console.log(button1);
+    })
+
+})
